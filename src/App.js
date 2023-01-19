4ef5ac09b4568781f20bd80a7eb7f5714de13313
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-01-20 01:48:12 +0900

    Authentication FE

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import Todo from "./Todo";
 import AddTodo from "./AddTodo";
 
 import React, { useEffect, useState } from "react";
-import { Container, List, Paper } from "@mui/material";
-import { call } from "./service/ApiService";
+import {
+  Container,
+  List,
+  Paper,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { call, signout } from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null).then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => {
+      setItems(response.data);
+      setLoading(false);
+    });
   }, []);
 
   const addItem = (item) => {
@@ -40,14 +53,41 @@ function App() {
     </Paper>
   );
 
-  return (
-    <div className="App">
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid justifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할 일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
+  let todoListPage = (
+    <div>
+      {navigationBar}
       <Container maxWidth="md">
-        <AddTodo addItem={addItem}></AddTodo>
+        <AddTodo addItem={addItem} />
         <div className="TodoList">{todoItems}</div>
       </Container>
     </div>
   );
+
+  let loadingPage = <h1>로딩 중...</h1>;
+  let content = loadingPage;
+
+  if (!loading) {
+    content = todoListPage;
+  }
+
+  return <div className="App">{content}</div>;
 }
 
 export default App;
---
+++ src/AppRouter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./index.css";
+import App from "./App.js";
+import Login from "./Login.js";
+import SignUp from "./SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright ⓒ "}
+      fsoftwareengineer, {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function AppRouter() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </div>
+  );
+}
+
+export default AppRouter;
