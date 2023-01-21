7964fb14508f984749ca555510229523bfb40c39
helen1031 <32950301+helen1031@users.noreply.github.com>
2023-01-22 06:15:24 +0900

    release AWS FE

--- src/api-config.js
+++ src/api-config.js
@@ -4,6 +4,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   backendHost = "http://localhost:8080";
+} else {
+  backendHost =
+    "http://prod-todo-sideproject-hj-2023.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
